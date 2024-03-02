52d50d3f2cbc2161d4f07048431be1b2dd758b29
bansemer <bansemer@ucar.edu>
2024-03-02 02:13:53 -0700

    i

--- holoDiagnostics_script.m
+++ holoDiagnostics_script.m
@@ -2,22 +2,24 @@
 
 %% Directory/file locations for current flight, edit as needed
 % Hologram directories
-driveA = '/Volumes/Holodec05A/CAESER/RF02';
-driveB = '/Volumes/Holodec05B/CAESER/RF02';
+driveA = '/Volumes/Holodec05A/CAESER/RF02/';
+driveB = '/Volumes/Holodec05B/CAESER/RF02/';
 
 % Holodec housekeeping directory (leave empty if unavailable)
-housedir = '/Volumes/Holodec05A/CAESER/20240229_data';
+housedir = '/Volumes/Holodec05A/CAESER/20240229_data/';
 
 % LRT data file (leave empty if unavailable)
 ncfile = '/Users/bansemer/data/caesar/c130/CAESARrf02.nc';
 
 %% Compare A and B drives
-if driveA(end) ~= filesep; driveA = [driveA filesep]; end
-if driveB(end) ~= filesep; driveB = [driveB filesep]; end
-driveAholograms = dir([driveA '**/*.tiff']);
-driveBholograms = dir([driveB '**/*.tiff']);
-disp(['Number of DriveA holograms: '+string(length(driveAholograms))]);
-disp(['Number of DriveB holograms: '+string(length(driveBholograms))]);
+if exist(driveA) && exist(driveB)
+    if driveA(end) ~= filesep; driveA = [driveA filesep]; end
+    if driveB(end) ~= filesep; driveB = [driveB filesep]; end
+    driveAholograms = dir([driveA '**/*.tiff']);
+    driveBholograms = dir([driveB '**/*.tiff']);
+    disp(['Number of DriveA holograms: '+string(length(driveAholograms))]);
+    disp(['Number of DriveB holograms: '+string(length(driveBholograms))]);
+end
 
 %% Run diagnostics
 fn = holoDiagnostics(driveA, 'housedir', housedir, 'ncfile', ncfile);
